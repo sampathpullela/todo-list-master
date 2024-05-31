d4be92e0b557fb2d049b79eebe2dbf320994b37d
sampathpullela <sampathpullela99@gmail.com>
2024-05-31 14:56:34 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('focus', function(){
+        paragraph.style.color = "red";
+console.log('paragraph');
+    })
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+    })
+})
+
